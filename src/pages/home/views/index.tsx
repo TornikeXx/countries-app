095cdcb8b84ff8af98bc35d3ec1354d9896e393b
TornikeXx <toko.tabatadze2@gmail.com>
2024-10-02 20:46:07 +0400

    changed few lazy import

--- src/pages/home/views/index.tsx
+++ src/pages/home/views/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import Hero from '../../../components/Hero/Hero'
 import Card from '../components/Card/Card'
-import { lazy } from 'react'
-import { Suspense } from 'react'
+import CardHeader from '../components/Card-header/Card-header'
+import CardContent from '../components/Card-content/Card-content'
+
 
 interface Country {
     name: string;
@@ -18,23 +19,13 @@ const HomePageView: React.FC = () => {
         { name:"Italy", capital:"Rome", population:"67 million", image:"https://lp-cms-production.imgix.net/2020-12/LPT0717_078.jpg?fit=crop&w=360&ar=1%3A1&auto=format&q=75"}
   ]    
   
-  const LazyCardHeader = lazy(
-    () => import("../components/Card-header/Card-header")
-  );
-  const LazyCardContent = lazy(
-    ()=> import("../components/Card-content/Card-content")
-  )
   
   return (
       <>
       <Hero title='Discover story-worthy travel moments' />
       <Card>
-        <Suspense fallback={<div>Loading Header...</div>}>
-          <LazyCardHeader />
-        </Suspense>
-        <Suspense fallback={<div>Loading Content...</div>}>
-          <LazyCardContent countries={countries}/>
-        </Suspense>
+          <CardHeader />
+          <CardContent countries={countries}/>
       </Card>
       
     </>
