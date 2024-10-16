ca5aed028b8aa37f69f79237411d89a4de14e9fd
TornikeXx <toko.tabatadze2@gmail.com>
2024-10-16 18:35:31 +0400

    Refactor forms to controlled components and add input validation

--- src/pages/home/components/list/Card-creating-form/Card-creating-form.tsx
+++ src/pages/home/components/list/Card-creating-form/Card-creating-form.tsx
@@ -1,19 +1,66 @@
 import styles from "./Card-creating-form.module.css"
-import { FormEvent } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 
 type CountryCreateFormProps = {
-  onCountryCreate: (e: FormEvent<HTMLFormElement>) => void;
+  onCountryCreate: (articleFields: { name: string, capital:string, population:string}) => void;
 };
-const CardForm:React.FC<CountryCreateFormProps> = ({onCountryCreate}) => {
+const CardForm: React.FC<CountryCreateFormProps> = ({ onCountryCreate }) => {
+
+  const [name, setName] = useState("")
+  const [capital, setCapital] = useState("")
+  const [population, setPopulation] = useState("")
+
+  const[nameError,setNameError] = useState("")
+  const[capitalError,setCapitalError] = useState("")
+  const[populationError,setPopulationError] = useState("")
+  
+  const handleChangeName = (e:ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value
+    if (name.length > 10) {
+      setNameError("max 10 characters")
+    } else {
+      setNameError("")
+    }
+    setName(value)
+  }
+  const handleChangeCapital = (e:ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value
+    if (capital.length > 10) {
+      setCapitalError("max 10 characters")
+    } else {
+      setCapitalError("")
+    }
+    setCapital(value)
+  }
+  const handleChangePopulation = (e:ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value
+    if (isNaN(Number(value))) {
+      setPopulationError("only numbers")
+    } else {
+      setPopulationError("")
+    }
+    setPopulation(value)
+  }
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    onCountryCreate({ name, capital, population })
+    setName("")
+    setCapital("")
+    setPopulation("")
+  }
   return (
-      <form className={styles.wrapper}
-      onSubmit={onCountryCreate}
+    <form className={styles.wrapper}
+      onSubmit={handleSubmit}
       >
-          <input name="name" placeholder="name" />
-          <input name="capital" placeholder="capital" />
-          <input name="population" placeholder="populaton" />
-          <button type="submit">Add</button>
-    </form>
+      <input name="name" placeholder="name" value={name} onChange={handleChangeName} />
+      <span>{nameError}</span>
+      <input name="capital" placeholder="capital" value={capital} onChange={handleChangeCapital} />
+      <span>{capitalError }</span>
+      <input name="population" placeholder="populaton" value={population} onChange={handleChangePopulation} />
+      <span>{ populationError}</span>
+      <button type="submit">Add</button>
+  </form>
   )
 }
 
